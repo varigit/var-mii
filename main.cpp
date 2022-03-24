0054091469889ff1a8bc9d2af5a7b9a1a172167f
Nate Drude <nate.d@variscite.com>
2022-03-24 10:31:14 -0500

    Add mii_write_reg function
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- main.cpp
+++ main.cpp
@@ -14,10 +14,11 @@
 #define RET_ERROR -3
 
 void usage() {
-    printf("Usage:\n\n./var-mii <interface> <address> <register>\n\n");
+    printf("Usage:\n\n./var-mii <interface> <address> <register> <value>\n\n");
     printf("  interface: eth0, eth1, etc.\n");
     printf("  address: Phy Address\n");
     printf("  register: Optional argument to skip test and instead read a register\n");
+    printf("  value: Optional argument to skip test and instead write a register\n");
     printf("\nExamples:\n\n");
     printf("SOM:\t\t\t./var-mii eth0 0x04 0x2\n");
     printf("Symphony:\t\t./var-mii eth1 0x05 0x2\n");
@@ -59,6 +60,39 @@ int mii_read_reg(const char * if_name, const int phy_addr, const int phy_reg, __
     return ret;
 }
 
+int mii_write_reg(const char * if_name, const int phy_addr, const int phy_reg, const __u16 phy_val) {
+    struct ifreq ifr;
+    int ret = 0;
+
+    memset(&ifr, 0, sizeof(ifr));
+    strcpy(ifr.ifr_name, if_name);
+
+    struct mii_ioctl_data* mii = (struct mii_ioctl_data*)(&ifr.ifr_data);
+    mii->phy_id  = phy_addr;
+    mii->reg_num = phy_reg;
+    mii->val_in  = phy_val;
+    mii->val_out = 0;
+
+    const int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd != -1)
+    {
+        if (ioctl(fd, SIOCSMIIREG, &ifr) != -1) {
+            #if DEBUG
+            printf("%s: %s phy@%d reg 0x%x = 0x%x\n", __func__, if_name, phy_addr, phy_reg, mii->val_in);
+            #endif
+        } else {
+            printf("%s: Error: %s phy@%d reg 0x%x ioctl failed\n", __func__, if_name, phy_addr, phy_reg);
+            ret = -2;
+        }
+        close(fd);
+    } else {
+            printf("%s: Error: %s phy@%d reg 0x%x socket failed\n", __func__, if_name, phy_addr, phy_reg);
+            ret = -1;
+    }
+
+    return ret;
+}
+
 int var_verify_adin1300() {
     return 0;
 }
@@ -70,9 +104,9 @@ int var_verify_ar803x() {
 int main(int argc, char *argv[])
 {
     struct ifreq ifr;
-    __u16 phy_id;
     const char * if_name;
     int phy_addr, phy_reg = -1;
+    __u16 phy_val = 0xffff;
 
     /* parse args */
     if(argc == 3) {
@@ -82,24 +116,34 @@ int main(int argc, char *argv[])
         if_name = argv[1];
         phy_addr = strtol(argv[2], NULL, 0);
         phy_reg = strtol(argv[3], NULL, 0);
+    } else if(argc == 5) {
+        if_name = argv[1];
+        phy_addr = strtol(argv[2], NULL, 0);
+        phy_reg = strtol(argv[3], NULL, 0);
+        phy_val = strtol(argv[4], NULL, 0);
     } else {
         usage();
         return RET_BAD_ARGS;
     }
 
+    /* If value arg passed, write this register */
+    if(phy_val != 0xffff) {
+        if(mii_write_reg(if_name, phy_addr, phy_reg, phy_val))
+            return RET_IO_ERR;
+    }
     /* If register arg passed, read this register and exit */
     if(phy_reg >= 0) {
-        if(mii_read_reg(if_name, phy_addr, phy_reg, &phy_id))
+        if(mii_read_reg(if_name, phy_addr, phy_reg, &phy_val))
             return RET_IO_ERR;
-        printf("%x\n", phy_id);
+        printf("%x\n", phy_val);
         return 0;
     }
 
     /* Run Variscite PHY verification */
-    if(mii_read_reg(if_name, phy_addr, MII_PHYSID1, &phy_id))
+    if(mii_read_reg(if_name, phy_addr, MII_PHYSID1, &phy_val))
         return RET_IO_ERR;
 
-    switch(phy_id) {
+    switch(phy_val) {
         case 0x283:
             printf("PHY@%d: ADIN1300\n", phy_addr);
             var_verify_adin1300();
