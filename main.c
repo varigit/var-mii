75927a0d484616ce85813cb2f4104b9519c0a8e4
Alifer Moraes <alifer.m@variscite.com>
2024-08-30 16:22:24 -0300

    var-mii: add support for mxl86110 on DART-MX8M-PLUS
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

--- main.c
+++ main.c
@@ -55,6 +55,7 @@ machine_phyconfig_t machine_config_imx8mp = {
 	.phy_count = 2,
 	.phy_configs = {
 		/* dt8mcustomboard */
+		{ .phy = { .if_name = "eth0", .addr = 0, .id = MXL86110_PHY_ID_1, .mode = "rgmii" }},
 		{ .phy = { .if_name = "eth0", .addr = 0, .id = ADIN1300_PHY_ID_1, .mode = "rgmii" }},
 		{ .phy = { .if_name = "eth1", .addr = 1, .id = ADIN1300_PHY_ID_1, .mode = "rgmii" }},
 		{ .phy = { .if_name = "eth0", .addr = 0, .id = AR803x_PHY_ID_1,   .mode = "rgmii" }, .ar803_vddio = AT803X_VDDIO_1P8V },
