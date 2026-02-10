a9fe730c57b7d9cb1fc73ad1b3cb8e36795fa1c3
FrancescoFerraro <francesco.f@variscite.com>
2026-02-10 13:14:32 +0100

    var-mii: add support for DP83867 on VAR-SMARC-MX8MP-PLUS
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- main.c
+++ main.c
@@ -80,6 +80,9 @@ machine_phyconfig_t machine_config_imx8mp = {
 		{ .phy = { .if_name = "eth1", .addr = 5, .id = ADIN1300_PHY_ID_1, .mode = "rgmii" }},
 		{ .phy = { .if_name = "eth0", .addr = 4, .id = AR803x_PHY_ID_1,   .mode = "rgmii" }, .ar803_vddio = AT803X_VDDIO_1P8V },
 		{ .phy = { .if_name = "eth1", .addr = 5, .id = AR803x_PHY_ID_1,   .mode = "rgmii" }, .ar803_vddio = AT803X_VDDIO_1P8V },
+		/* echo */
+		{ .phy = { .if_name = "eth0", .addr = 4, .id = DP83867_PHY_ID_1, .mode = "rgmii" }},
+		{ .phy = { .if_name = "eth1", .addr = 5, .id = DP83867_PHY_ID_1, .mode = "rgmii" }},
 		/* last entry */
 		{ .phy = { .if_name = NULL }},
 	},
